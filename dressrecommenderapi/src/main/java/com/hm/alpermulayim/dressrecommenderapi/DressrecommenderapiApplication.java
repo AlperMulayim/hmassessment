338d68f409ff84cb7c86926c6fe78c42a97c30b7
alpermulayim <alper.mulayim48@gmail.com>
2024-07-14 10:39:03 +0300

    spring project init with  mysql, rest, lombok

---
+++ dressrecommenderapi/src/main/java/com/hm/alpermulayim/dressrecommenderapi/DressrecommenderapiApplication.java
@@ -0,0 +1,13 @@
+package com.hm.alpermulayim.dressrecommenderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DressrecommenderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DressrecommenderapiApplication.class, args);
+	}
+
+}
---
+++ dressrecommenderapi/src/test/java/com/hm/alpermulayim/dressrecommenderapi/DressrecommenderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hm.alpermulayim.dressrecommenderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DressrecommenderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
